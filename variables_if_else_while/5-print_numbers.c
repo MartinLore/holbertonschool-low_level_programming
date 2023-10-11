df4499fde20f423f5085b3a431538989dd072e9c
MartinLore <martin1342morales@gmail.com>
2023-10-11 13:45:23 -0300

    file added

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * main - The above function prints the numbers 0 to 9 on separate lines.
+ * 
+ * Return: The main function is returning an integer value of 0.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d", i);
+    }
+    printf("\n");
+
+    return (0);
+}
