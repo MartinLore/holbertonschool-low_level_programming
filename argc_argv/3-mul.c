db847350489653350ac703e1e57f2f4f92772747
MartinLore <martin1342morales@gmail.com>
2023-10-31 09:38:46 -0300

    file added

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of pointers to the strings (arguments)
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+    int i, j, k;
+
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    else
+    {
+        i = atoi(argv[1]);
+        j = atoi(argv[2]);
+        k = i * j;
+        printf("%d\n", k);
+        return (0);
+    }
+}
