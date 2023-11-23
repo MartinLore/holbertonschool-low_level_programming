a0fd2b75d765cae89d1bae500d14ffac5bc805e9
MartinLore <martin1342morales@gmail.com>
2023-11-23 10:53:10 -0300

    file added

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to head of list_t list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+    list_t *temp;
+
+    while (head)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->str);
+        free(temp);
+    }
+}
